05c28cc53ef9a3eb41b8b86ca5731f98eedb5e17
hsisco <hysensisco@gmail.com>
2020-07-09 16:58:06 -0400

    Update player.py

--- src/player.py
+++ src/player.py
@@ -4,13 +4,3 @@ class Player:
     def __init__(self, name, location):
         self.name = name
         self.location = location
-
-    def move_rooms(self, direction):
-        room = self.location
-        move = f"{direction}_to"
-        if hasattr(room, move):
-            self.location = getattr(room, move)
-            return True
-        else:
-            print("You've hit a dead end. Try another direction.")
-            return False

6d99a4618c4ab1930f69a957d20d2fcc987944ff
hsisco <hysensisco@gmail.com>
2020-07-09 16:14:57 -0400

    Update player.py

--- src/player.py
+++ src/player.py
@@ -1,2 +1,16 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location
+
+    def move_rooms(self, direction):
+        room = self.location
+        move = f"{direction}_to"
+        if hasattr(room, move):
+            self.location = getattr(room, move)
+            return True
+        else:
+            print("You've hit a dead end. Try another direction.")
+            return False

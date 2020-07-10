72b88357b915860d21ce6544de25ed3141b8e5aa
hsisco <hysensisco@gmail.com>
2020-07-10 17:41:49 -0400

    Create item.py

---
+++ src/item.py
@@ -0,0 +1,8 @@
+class Item:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    # def what(self):
+    #     if len()
+    #     print(f"You have:\n{self.name}")
